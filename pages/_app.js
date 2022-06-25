6d0aa31b611ada2c78854597e358fced948871f6
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-06-25 16:11:16 +0100

    Added navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import Link from "next/link";
+import "../styles/globals.css";
+import styles from "../styles/Home.module.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header className={styles.header}>
+        <ul>
+          <li>
+            <Link href="/">Transform GIF</Link>
+          </li>
+          <li>
+            <Link href="/setDelayPage">Delay between frames</Link>
+          </li>
+          <li>
+            <Link href="/singleFramePage">Single frame</Link>
+          </li>
+          <li>
+            <Link href="/gifToVideoPage">GIF to Video</Link>
+          </li>
+        </ul>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
